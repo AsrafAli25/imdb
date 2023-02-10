4410112e172255bbccf8e10905e8cee8f96f18bf
Asraf Ali <asraf@theqa.in>
2023-02-10 12:26:05 +0530

    created the navbar component

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,7 @@
 
 const About = () => {
   return (
-    <div className="max-w-6xl mx-auto space-y-4">  
+    <div className="max-w-6xl mx-auto space-y-4 p-4">  
       <h1 className="text-2xl font-medium text-amber-600">About</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus provident sint fugiat consequatur debitis temporibus unde doloribus voluptatum illo deserunt! Architecto facere at eveniet. Eius illo dolores odit temporibus aperiam voluptate at recusandae! Minima delectus sint, perferendis aliquam corporis saepe harum illum reiciendis repudiandae non vitae deserunt impedit atque? Nulla animi aut nostrum numquam, quia hic commodi voluptates enim est repellendus voluptatem delectus, omnis officiis dolor quibusdam in nesciunt! Molestias asperiores pariatur dolore nostrum ullam debitis ipsum officia magni natus voluptate vitae neque doloribus odio atque, animi, porro voluptates! Maxime illo dolore consequuntur. Debitis numquam expedita atque culpa, optio repudiandae?
